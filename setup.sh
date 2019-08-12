52dc321a0fc659629acae0d481ace4b6736cdc03
viallikavoo <30293428+viallikavoo@users.noreply.github.com>
2019-08-12 14:33:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@ cd $HOME/Documents/Workspace/
 git clone https://github.com/dracula/terminal-app.git
 git clone https://github.com/powerline/fonts.git
 cd fonts && ./install.sh
-cd $HOME
-git clone https://github.com/viallikavoo/zsh-config.git
 cd $HOME/Documents/Workspace/
 ssh-keygen -t rsa -b 4096 -C "vialli_kavoo@hotmail.com"
 cd $HOME
@@ -12,6 +10,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/viallikavoo/zsh-config/mas
 cd .oh-my-zsh/tools
 cd $HOME/.oh-my-zsh/custom/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions
+cd $HOME
+git clone https://github.com/viallikavoo/zsh-config.git
 mv zsh-config/zshrc .oh-my-zsh/
 rm -rf .zshrc
 rm -rf zsh-config
