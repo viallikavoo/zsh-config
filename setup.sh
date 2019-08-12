c4112a0a41aa970a1a6a54da8075596c7ceced69
viallikavoo <30293428+viallikavoo@users.noreply.github.com>
2019-08-12 14:26:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@ git clone https://github.com/powerline/fonts.git
 cd fonts && ./install.sh
 cd $HOME
 git clone https://github.com/viallikavoo/zsh-config.git
-cd .oh-my-zsh/tools
-cd $HOME/.oh-my-zsh/custom/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions
 cd $HOME/Documents/Workspace/
 ssh-keygen -t rsa -b 4096 -C "vialli_kavoo@hotmail.com"
 cd $HOME
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &>
+cd .oh-my-zsh/tools
+cd $HOME/.oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions
 mv zsh-config/zshrc .oh-my-zsh/zshrc
 rm -rf .zshrc
 rm -rf zsh-config
