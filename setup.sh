385f55fcc32476ee9ba9e3e816932274dba8e5ac
viallikavoo <30293428+viallikavoo@users.noreply.github.com>
2019-08-23 14:21:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ git clone https://github.com/dracula/terminal-app.git
 git clone https://github.com/powerline/fonts.git
 cd fonts && ./install.sh
 cd $HOME/Documents/Workspace/
-ssh-keygen -t rsa -b 4096 -C "vialli_kavoo@hotmail.com"
+HOSTNAME=`hostname` ssh-keygen -t rsa -C "$HOSTNAME" -f "$HOME/Documents/Workspace/github" -P "" && cat $HOME/Documents/Workspace/github.pub
 cd $HOME
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/viallikavoo/zsh-config/master/tools/install.sh)"
 cd .oh-my-zsh/tools
@@ -16,3 +16,9 @@ mv zsh-config/zshrc .oh-my-zsh/
 rm -rf .zshrc
 rm -rf zsh-config
 ln -s .oh-my-zsh/zshrc .zshrc
+git clone git@github.com:viallikavoo/.vim.git
+rm .vimrc
+ln -s .vim/vimrc .vimrc
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install jq
+brew install awscli
